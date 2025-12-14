23cc56bda67ce487dddb80e7c8e15c73bd1670c6
DE-Nyel Namgyal <12230006.gcit@rub.edu.bt>
2025-12-14 21:41:23 +0600

    ddd

--- frontend/src/pages/Admin/AllProjects.jsx
+++ frontend/src/pages/Admin/AllProjects.jsx
@@ -3,13 +3,7 @@ import DashboardLayout from "../../components/Layout/DashboardLayout";
 const AllProjects = () => {
   return (
     <DashboardLayout>
-      <div className="p-8">
-        <h1 className="text-2xl font-bold mb-4">Admin Dashboard</h1>
-        <p className="text-gray-600">
-          Welcome to the Fund Manager Admin Dashboard
-        </p>
-        <div>AllProjects</div>
-      </div>
+      <div>AllProjects</div>
     </DashboardLayout>
   );
 };
