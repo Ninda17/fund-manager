5cda1f06a2bb34b6f5e559f879da0aedc0369105
DE-Nyel Namgyal <12230006.gcit@rub.edu.bt>
2026-01-05 14:02:13 +0600

    latest changes

--- frontend/src/pages/Admin/AllProjects.jsx
+++ frontend/src/pages/Admin/AllProjects.jsx
@@ -408,10 +408,10 @@ const MyProjects = () => {
                         Finance Personnel
                       </th>
                       <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        Amount
+                        Total Budget
                       </th>
                       <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        Expenses
+                        Total Expenditure
                       </th>
                       <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                         Utilization
